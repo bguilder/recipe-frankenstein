5980bc8d586a7e6b6e14ef6aa44f0d95dbea5fc0
Brian Guilder <bguilder@gmail.com>
2019-12-02 22:13:26 -0500

    basic sort ingredients by word frequency

--- frank_server/main.go
+++ frank_server/main.go
@@ -6,11 +6,15 @@ import (
 	"frank_server/runner"
 	"frank_server/scraper/allrecipes"
 	"frank_server/utils"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
+	"unicode"
 )
 
-const recipeName = "christmas cookies"
-const numberOfRecipes = 3
+const recipeName = "chicken and dumplings"
+const numberOfRecipes = 5
 
 func main() {
 	fmt.Printf("Searching AllRecipes for: %s\n\n", recipeName)
@@ -30,10 +34,94 @@ func main() {
 		time.Sleep(2 * time.Second)
 	}
 
-	fmt.Printf("\n\n\n=================================Total ingredients list!=================================\n\n\n")
+	allIngredients := []string{}
 	for i := 0; i < len(recipes); i++ {
-		for x := 0; x < len(recipes[i].Ingredients)-1; x++ {
-			fmt.Printf("- %s\n", recipes[i].Ingredients[x])
+		for x := 0; x < len(recipes[i].Ingredients); x++ {
+			allIngredients = append(allIngredients, recipes[i].Ingredients[x])
 		}
 	}
+	calculateWordFrequency(allIngredients)
 }
+
+func hasOmittedWord(word string) bool {
+	omittedWords := []string{"cup", "cups", "ounces", "ounce", "tablespoon", "tablespoons"}
+	for i := 0; i < len(omittedWords); i++ {
+		if strings.Contains(word, omittedWords[i]) {
+			return true
+		}
+		if !unicode.IsLetter([]rune(word)[0]) {
+			return true
+		}
+	}
+	return false
+}
+
+func calculateWordFrequency(allIngredients []string) {
+
+	allWordsDict := map[string]int{}
+	allIngredientsArr := []string{}
+	// split string
+	for i := 0; i < len(allIngredients); i++ {
+		splitString := strings.Split(allIngredients[i], " ")
+
+		// remove numbers from the slice
+		for x := 0; x < len(splitString); x++ {
+			if _, err := strconv.Atoi(splitString[x]); err == nil {
+				splitString = append(splitString[:x], splitString[x+1:]...)
+			}
+		}
+
+		// remove certain words from slice
+		for x := 0; x < len(splitString); x++ {
+			if hasOmittedWord(splitString[x]) {
+				splitString = append(splitString[:x], splitString[x+1:]...)
+				x--
+			}
+		}
+
+		combineString := strings.Join(splitString, " ")
+		allIngredientsArr = append(allIngredientsArr, combineString)
+
+		for x := 0; x < len(splitString); x++ {
+			if _, ok := allWordsDict[splitString[x]]; ok {
+				allWordsDict[splitString[x]]++
+			} else {
+				allWordsDict[splitString[x]] = 1
+			}
+		}
+	}
+
+	fmt.Printf("\n\n\n=================================Total ingredients list!=================================\n\n\n")
+
+	for i := 0; i < len(allIngredientsArr); i++ {
+		fmt.Printf("- %v\n", allIngredientsArr[i])
+	}
+	fmt.Printf("\n\n\n=================================Ingredients By Frequency!=================================\n\n\n")
+
+	pairList := rankByWordCount(allWordsDict)
+	for i := 0; i < len(pairList); i++ {
+		fmt.Printf("ingredient: %v - %v\n", pairList[i].Key, pairList[i].Value)
+	}
+}
+
+func rankByWordCount(wordFrequencies map[string]int) PairList {
+	pl := make(PairList, len(wordFrequencies))
+	i := 0
+	for k, v := range wordFrequencies {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sort.Reverse(pl))
+	return pl
+}
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
